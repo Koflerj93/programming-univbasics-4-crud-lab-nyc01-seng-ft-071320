82bc121ced5f6d8b0efa8ef498dc05e855d917c1
Joseph Kofler <koflerj93@gmail.com>
2020-07-07 17:07:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,15 @@
 def create_an_empty_array
-  
+   array = []
 end
 
 def create_an_array
-  
+  food_array = ["Pizza", "Ice Cream", "Pho", "Sushi"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+ array_of_words = ["blank", "this", "copy", "that"]
+ array_of_words.push("pool")
+
 end
 
 def add_element_to_start_of_array(array, element)
